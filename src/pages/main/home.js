906131577b45c8206d4fbff85a471e417575a2c5
fengxl <fengxl@chinawiserv.com>
2019-01-04 16:26:56 +0800

    添加动态页面

--- src/pages/main/home.js
+++ src/pages/main/home.js
@@ -1,34 +1,12 @@
 loader.define(function(require,exports,module) {
     
     var pageview = {},  // 页面模块
-        uiList,         // 列表控件
         uiSwipeApp,  //列表  
-        uiListviewMessage,      //消息侧滑菜单
+        uiListviewMessage,
         mainHeight = $(window).height() - $("#tabDynamicNav").height() - $(".bui-bar-side").height();
 
     // 页面初始化
     pageview.init = function () {
-        
-        // 初始化列表刷新加载
-        // uiList = bui.list({
-        //     id: "#uiTabScroll",
-        //     url: "http://www.easybui.com/demo/json/chuangyi/article-list.json",
-        //     data: {},
-        //     pageSize:10,
-        //     field: {
-        //         page: "page",        // 分页字段
-        //         size: "pageSize",    // 页数字段
-        //         data: ""         // 数据
-        //     },
-        //     template: template,
-        //     onLoad: function (scroll) {
-        //         // 自定义渲染
-        //     },
-        //     callback: function (e) {
-        //         // 点击单行回调 console.log($(this).text())
-        //     }
-        // });
-
         //下拉进入使用过的小程序
         uiSwipeApp = bui.swipe({
             id: "#uiSwipe",
@@ -42,50 +20,33 @@ loader.define(function(require,exports,module) {
         //初始化消息的侧滑菜单
         uiListviewMessage = bui.listview({
             id   : "#listview",
-            data : [
-                    {text: "置顶",classname : "primary"},
-                    {text: "删除",classname : "danger"}
-                    ],
-            width : 200,
-            callback : function(e,ui) {
-
-                var text = $(this).text().trim();
-
-                    if( text == '删除' ){
-                        // 给父层增加一个样式,便于控制删除
-                        $(this).parents(".list-item").fadeOut(300,function () {
-                            $(this).remove();
-                        });
-                    }
-                    
-                // 关闭侧滑
-                ui.close();
+            data : [{text: "置顶",classname : "primary"},
+                    {text: "删除",classname : "danger"}],
+            position:"right",
+            callback : function(e) {
+                var $this = $(e.target)
+                var text = $this.text().trim();
+                if( text == '删除' ){
+                    bui.confirm("确定要删除吗",function (e) {
+                        var text2 = $(e.target).text();
+                        if( text2 == "确定"){
+                            $this.parents("li").fadeOut(300,function () {
+                                $(this).remove();
+                            });
+                        }
+                    })
+                }
+                $this.close();
             }
         });
+        uiListviewMessage.open({
+            index:1
+        })
+        setTimeout(function (argument) {
+            uiListviewMessage.close()
+        },5000)
 
     }
-
-    //生成列表的模板
-    function template (data) {
-    
-        var html = "";
-            $.each(data,function(index, el) {
-                html +='<li class="bui-btn bui-box-align-top">';
-                html +='    <div class="thumbnail"><img src="applogo.png" alt=""></div>';
-                html +='    <div class="span1">';
-                html +='        <h3 class="item-title">'+el.Name+'</h3>';
-                html +='        <div class="item-text bui-box">';
-                html +='            <div class="span1">'+el.ExInforSources+'</div>';
-                html +='            <span class="time"><i class="icon-">&#xe643;</i>23</span>';
-                html +='            <span class="time"><i class="icon-">&#xe680;</i>495</span>';
-                html +='            <span class="time"><i class="icon-">&#xe641;</i>28</span>';
-                html +='        </div>';
-                html +='    </div>            ';
-                html +='</li>';
-            });
-    
-        return html;
-    };
 
 
     // 初始化
---
+++ src/pages/moments/moments.js
@@ -0,0 +1,16 @@
+
+loader.define(function(require,exports,module) {
+
+    var pageview = {};
+
+    // 初始化定义
+    pageview.init = function () {
+        
+    }
+
+    // 初始化
+    pageview.init();
+
+    // 输出模块
+    return pageview;
+})
